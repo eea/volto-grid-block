5d78f3ee89668690252c4279dedc0fe17938718c
Miu Razvan <miu.razvan28@gmail.com>
2020-12-01 18:14:34 +0200

    Lint fixes

--- src/Widgets/ColorPickerWidget.jsx
+++ src/Widgets/ColorPickerWidget.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import loadable from '@loadable/component';
 import { Checkbox, Grid } from 'semantic-ui-react';
 import cx from 'classnames';
